6b5372ae112f1011d62aee7e772bf0062d1fc658
zts1993 <admin@zts1993.com>
2014-01-11 11:02:48 +0800

    Admin Group

---
+++ Application/Admin/Controller/IndexController.class.php
@@ -0,0 +1,12 @@
+<?php
+// 本类由系统自动生成，仅供测试用途
+namespace Admin\Controller;
+use Think\Controller;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        echo "hello ADMIN";
+    }
+}
