cca26b3f8342f6b06bdf2b73079cf4d32c607571
zts1993 <admin@zts1993.com>
2014-01-11 11:08:12 +0800

    Index merge with green2013

--- index.php
+++ index.php
@@ -1,26 +1,71 @@
 <?php
 
+if (version_compare(PHP_VERSION, '5.3.0', '<')) die('require PHP > 5.3.0 !'); //这个是TP3.2的需求,需要namespace
 
-if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
+/**
+ * 下面的内容自己决定
+ */
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+@set_time_limit(240);
 
 /**
  * 系统调试设置
  * 项目正式部署后请设置为false
  */
-define ( 'APP_DEBUG', true );
+define ('APP_DEBUG', true);
+
+/**定义网站根目录
+ *
+ */
+define("WEB_ROOT", dirname(__FILE__) . "/");
 
 /**
- * 应用目录设置
- * 安全期间，建议安装调试完成后移动到非WEB目录
- */
-define ( 'APP_PATH', './Application/' );
+ * 强制修正URL路径(一般不需要开启)
+ * 适用于反代一个二级目录并且不能替换部分文字的情况。(我说的就是捉急的iis~~~~)
+ * /
+//define('__APP__', '');
 
 
 /**
  * 缓存目录设置
  * 此目录必须可写，建议移动到非WEB目录
  */
-define ( 'RUNTIME_PATH', './Data/Temp/' );
+define ('RUNTIME_PATH', './Data/Temp/');
+
+/** 定义项目日志文件路径
+ *  此目录必须可写，建议移动到非WEB目录
+ */
+define('LOG_PATH', WEB_ROOT . "Data/Log/");
+
+/**定义Cache目录
+ *  此目录必须可写，建议移动到非WEB目录
+ */
+define('WEB_CACHE_PATH', WEB_ROOT . "Data/Cache/");
+
+/**系统备份数据库文件存放目录
+ *  此目录必须可写，建议移动到非WEB目录
+ */
+define("DB_Backup_PATH", WEB_ROOT . "Data/DBbackup/");
+
+/**
+ * 系统备份文件存放目录
+ */
+define("System_Backup_PATH", WEB_ROOT . "Data/Backup/");
+
+/**系统升级文件存放目录
+ */
+define("Upgrade_PATH", WEB_ROOT . "Data/Upgrade/");
+
+/**上传文件存放目录
+ */
+define("UploadDir", "Upload/");
+
+/**
+ * 应用目录设置
+ * 安全期间，建议安装调试完成后移动到非WEB目录
+ */
+define ('APP_PATH', './Application/');
+
 
 /**
  * 引入核心入口
