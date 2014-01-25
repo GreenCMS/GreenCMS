b5e20475b53f395770941e81df7f10e01175ccce
zts1993 <admin@zts1993.com>
2014-01-25 12:27:30 +0800

    Const  define

--- Application/Admin/Controller/LoginController.class.php
+++ Application/Admin/Controller/LoginController.class.php
@@ -8,8 +8,9 @@
  */
 
 namespace Admin\Controller;
-use Think\Controller;
 
+use Org\Net\IpLocation;
+use Think\Controller;
 
 class LoginController extends Controller
 {
@@ -20,8 +21,19 @@ class LoginController extends Controller
 
     public function index()
     {
+        $this->display();
+    }
+
+    public function login()
+    {
+        $ip = get_client_ip();
+        echo $ip;
+        $Ip = new IpLocation('UTFWry.dat'); // 实例化类 参数表示IP地址库文件
 
 
+        $area = $Ip->getlocation('203.34.5.66'); // 获取某个IP地址所在的位置IpLocation('UTFWry.dat'); // 实例化类 参数表示IP地址库文件$area = $Ip->getlocation('203.34.5.66'); // 获取某个IP地址所在的位置
+
+        $this->show(' ');
     }
 
 }
--- index.php
+++ index.php
@@ -23,54 +23,17 @@ error_reporting(E_ERROR | E_WARNING | E_PARSE);
  * 项目正式部署后请设置为false
  */
 define ('APP_DEBUG', true);
-define ('GreenStudio', true); //绿荫专业
+define ('GreenStudio', true); //绿荫专用
 define('BUILD_DIR_SECURE', false);
 /**定义网站根目录
  *
  */
 define("WEB_ROOT", dirname(__FILE__) . "/");
+
+
 require(WEB_ROOT . "db_config.php");
+require(WEB_ROOT . "const_config.php");
 
-/**
- * 强制修正URL路径(一般不需要开启)
- * 适用于反代一个二级目录并且不能替换部分文字的情况。(我说的就是捉急的iis~~~~)
- * /
-//define('__APP__', '');
-
-
-/**
- * 缓存目录设置
- * 此目录必须可写，建议移动到非WEB目录
- */
-define ('RUNTIME_PATH', './Data/Temp/');
-
-/** 定义项目日志文件路径
- *  此目录必须可写，建议移动到非WEB目录
- */
-define('LOG_PATH', WEB_ROOT . "Data/Log/");
-
-/**定义Cache目录
- *  此目录必须可写，建议移动到非WEB目录
- */
-define('WEB_CACHE_PATH', WEB_ROOT . "Data/Cache/");
-
-/**系统备份数据库文件存放目录
- *  此目录必须可写，建议移动到非WEB目录
- */
-define("DB_Backup_PATH", WEB_ROOT . "Data/DBbackup/");
-
-/**
- * 系统备份文件存放目录
- */
-define("System_Backup_PATH", WEB_ROOT . "Data/Backup/");
-
-/**系统升级文件存放目录
- */
-define("Upgrade_PATH", WEB_ROOT . "Data/Upgrade/");
-
-/**上传文件存放目录
- */
-define("UploadDir", "Upload/");
 
 /**
  * 应用目录设置
