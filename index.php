377b761cc94fa7ef110e3e229d8b8321174ec359
zts1993 <admin@zts1993.com>
2014-02-12 21:50:12 +0800

    version update

--- index.php
+++ index.php
@@ -42,8 +42,8 @@ require(WEB_ROOT . "const_config.php");
  */
 define ('APP_PATH', './Application/');
 
-define ('GreenCMS_Version', 'v2.1.0211');
-define ('GreenCMS_Build', '20140211');
+define ('GreenCMS_Version', 'v2.1.0212');
+define ('GreenCMS_Build', '20140212');
 
 /**
  * 引入核心入口
