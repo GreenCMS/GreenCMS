8fc87d9d813edf727b79565f1353bfbea58bf5a6
codesth <977135148@qq.com>
2014-02-20 19:00:04 +0800

    插件清理。

--- SQL Update/green_addons.sql
+++ SQL Update/green_addons.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: 127.0.0.1
--- 生成日期: 2014 年 02 月 16 日 12:34
+-- 生成日期: 2014 年 02 月 20 日 11:57
 -- 服务器版本: 5.5.27
 -- PHP 版本: 5.4.7
 
@@ -26,7 +26,6 @@ SET time_zone = "+00:00";
 -- 表的结构 `green_addons`
 --
 
-DROP TABLE IF EXISTS `green_addons`;
 CREATE TABLE IF NOT EXISTS `green_addons` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
   `name` varchar(40) NOT NULL COMMENT '插件名或标识',
@@ -46,14 +45,7 @@ CREATE TABLE IF NOT EXISTS `green_addons` (
 --
 
 INSERT INTO `green_addons` (`id`, `name`, `title`, `description`, `status`, `config`, `author`, `version`, `create_time`, `has_adminlist`) VALUES
-(15, 'EditorForAdmin', '后台编辑器', '用于增强整站长文本的输入和显示', 1, '{"editor_type":"2","editor_wysiwyg":"1","editor_height":"500px","editor_resize_type":"1"}', 'xjh1994', '0.1', 1383126253, 0),
-(2, 'SiteStat', '站点统计信息', '统计站点的基础信息', 1, '{"title":"\\u7cfb\\u7edf\\u4fe1\\u606f","width":"1","display":"1","status":"0"}', 'xjh1994', '0.1', 1379512015, 0),
-(3, 'DevTeam', '开发团队信息', '开发团队成员信息', 1, '{"title":"OneThink\\u5f00\\u53d1\\u56e2\\u961f","width":"2","display":"1"}', 'xjh1994', '0.1', 1379512022, 0),
-(4, 'SystemInfo', '系统环境信息', '用于显示一些服务器的信息', 1, '{"title":"\\u7cfb\\u7edf\\u4fe1\\u606f","width":"2","display":"1"}', 'xjh1994', '0.1', 1379512036, 0),
-(5, 'Editor', '前台编辑器', '用于增强整站长文本的输入和显示', 1, '{"editor_type":"2","editor_wysiwyg":"1","editor_height":"300px","editor_resize_type":"1"}', 'xjh1994', '0.1', 1379830910, 0),
-(6, 'Attachment', '附件', '用于文档模型上传附件', 1, 'null', 'xjh1994', '0.1', 1379842319, 1),
-(9, 'SocialComment', '通用社交化评论', '集成了各种社交化评论插件，轻松集成到系统中。', 1, '{"comment_type":"2","comment_uid_youyan":"1894186","comment_short_name_duoshuo":"greencmsduoshuo","comment_form_pos_duoshuo":"buttom","comment_data_list_duoshuo":"10","comment_data_order_duoshuo":"desc"}', 'xjh1994', '0.1', 1380273962, 0),
-(17, 'baidushare', '百度分享', '用户将网站内容分享到第三方网站，从第三方网站带来社会化流量。', 0, '{"openbutton":"1","openimg":"0","openselect":"0"}', 'xjh1994', '0.1', 1392444510, 0);
+(9, 'SocialComment', '通用社交化评论', '集成了各种社交化评论插件，轻松集成到系统中。', 1, '{"comment_type":"2","comment_uid_youyan":"1894186","comment_short_name_duoshuo":"greencmsduoshuo","comment_form_pos_duoshuo":"buttom","comment_data_list_duoshuo":"10","comment_data_order_duoshuo":"desc"}', 'xjh1994', '0.1', 1380273962, 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
