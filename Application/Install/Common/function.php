d6d72094f0fdd535a225996b423c41b4e4c8d55e
zts1993 <admin@zts1993.com>
2014-03-02 16:53:12 +0800

    small       fix

--- Application/Install/Common/function.php
+++ Application/Install/Common/function.php
@@ -49,43 +49,43 @@ function TestWrite($d)
     }
 }
 
+//
+//function delDir($dirName)
+//{
+//    if (!file_exists($dirName)) {
+//        return false;
+//    }
+//
+//    $dir = opendir($dirName);
+//    while ($fileName = readdir($dir)) {
+//        $file = $dirName . '/' . $fileName;
+//        if ($fileName != '.' && $fileName != '..') {
+//            if (is_dir($file)) {
+//                delDir($file);
+//            } else {
+//                unlink($file);
+//            }
+//        }
+//    }
+//    closedir($dir);
+//    return rmdir($dirName);
+//}
 
-function delDir($dirName)
-{
-    if (!file_exists($dirName)) {
-        return false;
-    }
 
-    $dir = opendir($dirName);
-    while ($fileName = readdir($dir)) {
-        $file = $dirName . '/' . $fileName;
-        if ($fileName != '.' && $fileName != '..') {
-            if (is_dir($file)) {
-                delDir($file);
-            } else {
-                unlink($file);
-            }
-        }
-    }
-    closedir($dir);
-    return rmdir($dirName);
-}
-
-
-function GetIP()
-{
-    if (getenv("HTTP_CLIENT_IP") && strcasecmp(getenv("HTTP_CLIENT_IP"), "unknown"))
-        $ip = getenv("HTTP_CLIENT_IP");
-    else if (getenv("HTTP_X_FORWARDED_FOR") && strcasecmp(getenv("HTTP_X_FORWARDED_FOR"), "unknown"))
-        $ip = getenv("HTTP_X_FORWARDED_FOR");
-    else if (getenv("REMOTE_ADDR") && strcasecmp(getenv("REMOTE_ADDR"), "unknown"))
-        $ip = getenv("REMOTE_ADDR");
-    else if (isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] && strcasecmp($_SERVER['REMOTE_ADDR'], "unknown"))
-        $ip = $_SERVER['REMOTE_ADDR'];
-    else
-        $ip = "unknown";
-    return ($ip);
-}
+//function GetIP()
+//{
+//    if (getenv("HTTP_CLIENT_IP") && strcasecmp(getenv("HTTP_CLIENT_IP"), "unknown"))
+//        $ip = getenv("HTTP_CLIENT_IP");
+//    else if (getenv("HTTP_X_FORWARDED_FOR") && strcasecmp(getenv("HTTP_X_FORWARDED_FOR"), "unknown"))
+//        $ip = getenv("HTTP_X_FORWARDED_FOR");
+//    else if (getenv("REMOTE_ADDR") && strcasecmp(getenv("REMOTE_ADDR"), "unknown"))
+//        $ip = getenv("REMOTE_ADDR");
+//    else if (isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] && strcasecmp($_SERVER['REMOTE_ADDR'], "unknown"))
+//        $ip = $_SERVER['REMOTE_ADDR'];
+//    else
+//        $ip = "unknown";
+//    return ($ip);
+//}
 
 
 function insertDB($file, $conn)
