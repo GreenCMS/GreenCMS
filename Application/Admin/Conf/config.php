f6f79398a98a051d90041f57acdd8c8ef7d3d808
zts1993 <admin@zts1993.com>
2014-03-18 21:22:51 +0800

    修复在没有pathinfo支持的情况下admin未登录跳转的问题。

--- Application/Admin/Conf/config.php
+++ Application/Admin/Conf/config.php
@@ -113,12 +113,12 @@ $config_admin = array(
      * RBAC认证配置信息
     */
     'USER_AUTH_ON'        => true,
-    'USER_AUTH_TYPE'      => 2, // 默认认证类型 1 登录认证 2 实时认证
+    'USER_AUTH_TYPE'      => 1 , // 默认认证类型 1 登录认证 2 实时认证
     'USER_AUTH_KEY'       => 'authId', // 用户认证SESSION标记
     'ADMIN_AUTH_KEY'      => 'ADMIN',
     'USER_AUTH_MODEL'     => 'User', // 默认验证数据表模型
     'AUTH_PWD_ENCODER'    => 'md5', // 用户认证密码加密方式encrypt
-    'USER_AUTH_GATEWAY'   => '/Admin/Login/index', // 默认认证网关
+    'USER_AUTH_GATEWAY'   => '?s=/Admin/Login/index', // 默认认证网关
     'NOT_AUTH_MODULE'     => 'Public', // 默认无需认证模块
     'REQUIRE_AUTH_MODULE' => '', // 默认需要认证模块
     'NOT_AUTH_ACTION'     => '', // 默认无需认证操作
