32a510ffb8637f71d987dc902a28640563a59c75
zts1993 <admin@zts1993.com>
2014-03-19 20:35:08 +0800

    20140319 build

--- const_config.php
+++ const_config.php
@@ -74,8 +74,8 @@ define("TIME_FIX", 0);
 /**
  * version code
  */
-define ('GreenCMS_Version', 'v2.1.0318');
-define ('GreenCMS_Build', '20140318');
+define ('GreenCMS_Version', 'v2.1.0319');
+define ('GreenCMS_Build', '20140319');
 
 /**
  * BUILD_DIR_SECURE 目录安全，设置详细参考thinkphp手册
