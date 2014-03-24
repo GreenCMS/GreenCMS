d143b0fd922dce21aa8377933f4b5b8328b6873d
zts1993 <admin@zts1993.com>
2014-03-24 18:19:12 +0800

    test2

--- version_config.php
+++ version_config.php
@@ -11,7 +11,7 @@
  * version code
  */
 define ('GreenCMS_Version', 'v2.1.0324');
-define ('GreenCMS_Build', '20140325');
+define ('GreenCMS_Build', '20140324');
 
 define('Server_API', "http://greenapi.sinaapp.com/");
 
