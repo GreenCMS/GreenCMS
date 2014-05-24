d4a16768f69cf11d17c781f6ee463ca8355959d9
zts1993 <zts1993@gmail.com>
2014-05-24 23:47:21 +0800

    学生处slider

--- Application/Home/View/Xsc/function.php
+++ Application/Home/View/Xsc/function.php
@@ -10,3 +10,28 @@ function hello_world()
     dump("自定义函数test");
 }
 
+
+//缩略post_img
+/**
+ * 缩略图获取
+ * @param $post
+ * @return string
+ */
+function get_post_img($post)
+{
+
+    if (!empty($post['post_img'])) {
+        return $post['post_img'];
+    } else {
+        $content = $post['post_content'];
+        preg_match_all('/<img.*?(?: |\\t|\\r|\\n)?src=[\'"]?(.+?)[\'"]?(?:(?: |\\t|\\r|\\n)+.*?)?>/sim', $content, $strResult, PREG_PATTERN_ORDER);
+        $n = count($strResult[1]);
+        $random = mt_rand(1, 20);
+        if ($n > 0) {
+            return $strResult[1][0];
+        } else {
+            return get_opinion('site_url') . '/Public/share/img/random/tb' . $random . '.jpg';
+
+        }
+    }
+}
