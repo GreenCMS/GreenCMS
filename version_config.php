ec0212d784baa3c98d8aa7d24bb0276a7d906747
zts1993 <zts1993@gmail.com>
2014-06-02 16:58:45 +0800

    version 20140602 (发布版本)

--- version_config.php
+++ version_config.php
@@ -10,8 +10,8 @@
 /**
  * version code
  */
-define ('GreenCMS_Version', 'v2.1.0601');
-define ('GreenCMS_Build', '20140601');
+define ('GreenCMS_Version', 'v2.1.0602');
+define ('GreenCMS_Build', '20140602');
 
 define('Server_API', "http://greenapi.sinaapp.com/");
 
