cc9d72523fb402f113ef1db89327420521cf313d
zts1993 <zts1993@gmail.com>
2014-08-14 23:54:24 +0800

    每个页面检查安全事件和升级事件

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -9,6 +9,7 @@
 
 namespace Admin\Controller;
 
+use Common\Event\AccessEvent;
 use Common\Event\UpdateEvent;
 use Common\Event\UserEvent;
 use Think\Storage;
@@ -53,6 +54,38 @@ class IndexController extends AdminBaseController
         }
 
         die($message);
+    }
+
+
+
+
+    public function checkTodo()
+    {
+        $cheack_res="";
+
+        $AccessEvent= new AccessEvent();
+        $UpdateEvent = new UpdateEvent();
+
+        if($UpdateEvent->check()){
+            $cheack_res.=  '<li><a href="' . U("Admin/System/update") . '"><i class="fa fa-laptop"></i> 发现新的可升级版本</a></li>';
+        }
+
+
+        if(!$AccessEvent->checkAccess()){
+            $cheack_res.='<li><a href="' . U("Admin/Access/rolelist") . '"><i class="fa fa-laptop"></i> 需要重建角色权限！</a></li>';
+
+        }
+
+        if(!$AccessEvent->checkNode()){
+            $cheack_res.='<li><a href="' . U("Admin/Access/nodelist") . '"><i class="fa fa-laptop"></i> 需要重建节点！</a></li>';
+
+
+        }
+
+
+        if($cheack_res=="") die("none");
+        die($cheack_res);
+
     }
 
 
