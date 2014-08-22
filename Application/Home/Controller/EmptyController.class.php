385f4359146e0cf641de5b90b7ce7e238372ffe0
zts1993 <zts1993@gmail.com>
2014-08-22 23:32:38 +0800

    small improvement

--- Application/Home/Controller/EmptyController.class.php
+++ Application/Home/Controller/EmptyController.class.php
@@ -16,58 +16,50 @@
 
 namespace Home\Controller;
 
-use Home\Controller\AddonsController;
-
-
 /**
  * 空控制器当访问出错时调用
  * Class EmptyController
  * @package Home\Controller
  */
-class EmptyController extends HomeBaseController
-{
-    /**
-     * 空控制器实现
-     * @param $method
-     * @param $args
-     * @return mixed|void
-     * @internal param $null
-     */
-    public function __call($method, $args)
-    {
 
-        $pluginRes=$this->anonymousPlugin(CONTROLLER_NAME, ACTION_NAME, I('get.plugin'));
+class EmptyController extends HomeBaseController {
+	/**
+	 * 空控制器实现
+	 * @param $method
+	 * @param $args
+	 * @return mixed|void
+	 * @internal param $null
+	 */
+	public function __call($method, $args) {
 
-        if($pluginRes){
-        }else{
-            $this->error404();
-        }
+		$pluginRes = $this->anonymousPlugin(CONTROLLER_NAME, ACTION_NAME, I('get.action'));
 
+		if ($pluginRes) {
+		} else {
+			$this->error404();
+		}
 
-    }
+	}
 
+	public function anonymousPlugin($_addons = null, $_controller = null, $_action = null) {
 
-    public function anonymousPlugin($_addons = null, $_controller = null, $_action = null)
-    {
+		if ($_action == null) {
+			return false;
+		}
 
+		if (C('URL_CASE_INSENSITIVE')) {
+			$_addons     = ucfirst(parse_name($_addons, 1));
+			$_controller = parse_name($_controller, 1);
+		}
 
+		if (!empty($_addons) && !empty($_controller) && !empty($_action)) {
 
-        if (C('URL_CASE_INSENSITIVE')) {
-            $_addons = ucfirst(parse_name($_addons, 1));
-            $_controller = parse_name($_controller, 1);
-        }
+			$Addons = A("Addons://{$_addons}/{$_controller}")->$_action();
+			return true;
+		} else {
+			return false;
+		}
 
-        if (!empty($_addons) && !empty($_controller) && !empty($_action)) {
-
-
-            $Addons = A("Addons://{$_addons}/{$_controller}")->$_action();
-
-        } else {
-            return false;
-        }
-
-
-    }
-
+	}
 
 }
