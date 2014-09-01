40b296a98ea5d7b6ab63356890166e330ac3d205
zts1993 <zts1993@gmail.com>
2014-09-01 20:51:45 +0800

    version 20140901 beta published-first

--- version_config.php
+++ version_config.php
@@ -10,8 +10,8 @@
 /**
  * version code
  */
-define ('GreenCMS_Version', 'v2.1.0822');
-define ('GreenCMS_Build', '20140822');
+define ('GreenCMS_Version', 'v2.1.0901');
+define ('GreenCMS_Build', '20140901');
 
 define('Server_API', "http://api.greencms.net/");
 
