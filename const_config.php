2a869f253d77d0a4887d034bc7590d7168349d8b
zts1993 <zts1993@gmail.com>
2014-12-03 16:25:52 +0800

    fix install

--- const_config.php
+++ const_config.php
@@ -35,13 +35,13 @@ define('WEB_CACHE_PATH', WEB_ROOT . "Data/Cache/");
  * 系统备份数据库文件存放目录
  * 此目录必须可写，建议移动到非WEB目录
  */
-define("DB_Backup_PATH", WEB_ROOT . "Data/DBbackup_5500c17502/");
+define("DB_Backup_PATH", WEB_ROOT . "Data/DBbackup/");
 
 /**
  * 系统备份文件存放目录
  * 此目录必须可写，建议移动到非WEB目录
  */
-define("System_Backup_PATH", WEB_ROOT . "Data/Backup_5500c17502/");
+define("System_Backup_PATH", WEB_ROOT . "Data/Backup/");
 
 /**
  * 系统升级文件存放目录
